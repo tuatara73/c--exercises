38cee8085a13ca1acc17b5ffe7791fb4d7c30ae9
Shiran Vatury <shiran.sv@gmail.com>
2022-10-17 16:14:37 +0300

    fixing typo

--- Program.cs
+++ Program.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 /*
 Question:
-Define a class which has at least two methods:
-getString: to get a string from console input
-printString: to print the string in upper case.
-Also please include simple test function to test the class methods.
+Write a program that calculates and prints the value according to the given formula:
+Q = Square root of [(2 * C * D)/H]
+Following are the fixed values of C and H:
+C is 50. H is 30.
+D is the variable whose values should be input to your program in a comma-separated sequence.
+Example
+Let us assume the following comma separated input sequence is given to the program:
+100,150,180
+The output of the program should be:
+18,22,24
 */
 namespace c_sharp_exercises
 {
@@ -13,7 +19,7 @@ namespace c_sharp_exercises
     {
         static void Main(string[] args)
         {
-            printString(z);
+            
         }
     }
 }
